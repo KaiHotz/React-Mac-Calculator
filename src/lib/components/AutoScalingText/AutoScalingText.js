54a330cf125a672113183616ecc23d650d3c6954
Kai Hotz <kaihotz@me.com>
2019-02-09 23:24:53 +0100

    added useScale hook

--- src/lib/components/AutoScalingText/AutoScalingText.js
+++ src/lib/components/AutoScalingText/AutoScalingText.js
@@ -1,26 +1,11 @@
-import React, {
-  useState, useRef, useEffect,
-} from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
+import { useScale } from '../../utils/hooks'
 import './styles.css'
 
 const AutoScalingText = ({ children }) => {
-  const [scale, setScale] = useState(1)
   const node = useRef()
-
-  useEffect(() => {
-    const { parentNode } = node ?.current
-    const availableWidth = parentNode.offsetWidth
-    const actualWidth = node ?.current.offsetWidth
-    const actualScale = availableWidth / actualWidth
-    if (scale === actualScale) { return }
-
-    if (actualScale < 1) {
-      setScale(actualScale)
-    } else if (scale < 1) {
-      setScale(1)
-    }
-  })
+  const scale = useScale(node)
 
   return (
     <div
