0a671584c54311bce57ab67cb105f9d30fe5d723
KaiHotz <kaihotz@me.com>
2022-05-09 09:49:38 +0400

    test semicolons added

--- src/Calculator/Calculator.test.tsx
+++ src/Calculator/Calculator.test.tsx
@@ -1,47 +1,47 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 import { Calculator } from './Calculator';
 
 describe('<Calculator />', () => {
-  const user = userEvent.setup()
+  const user = userEvent.setup();
   it('should sum correctly', async () => {
     render(<Calculator />);
 
-    await user.click(screen.getByText('6'))
-    await user.click(screen.getByText('+'))
-    await user.click(screen.getByText('2'))
-    await user.click(screen.getByText('='))
+    await user.click(screen.getByText('6'));
+    await user.click(screen.getByText('+'));
+    await user.click(screen.getByText('2'));
+    await user.click(screen.getByText('='));
 
     expect(screen.getByTestId('calculator-display-inner')).toHaveTextContent('8');
   });
 
   it('should rest correctly', async () => {
     render(<Calculator />);
-    await user.click(screen.getByText('6'))
-    await user.click(screen.getByText('-'))
-    await user.click(screen.getByText('2'))
-    await user.click(screen.getByText('='))
+    await user.click(screen.getByText('6'));
+    await user.click(screen.getByText('-'));
+    await user.click(screen.getByText('2'));
+    await user.click(screen.getByText('='));
 
     expect(screen.getByTestId('calculator-display-inner')).toHaveTextContent('4');
   });
 
   it('should divide correctly', async () => {
     render(<Calculator />);
-    await user.click(screen.getByText('6'))
-    await user.click(screen.getByText('÷'))
-    await user.click(screen.getByText('2'))
-    await user.click(screen.getByText('='))
+    await user.click(screen.getByText('6'));
+    await user.click(screen.getByText('÷'));
+    await user.click(screen.getByText('2'));
+    await user.click(screen.getByText('='));
 
     expect(screen.getByTestId('calculator-display-inner')).toHaveTextContent('3');
   });
 
   it('should multiply correctly', async () => {
     render(<Calculator />);
-    await user.click(screen.getByText('6'))
-    await user.click(screen.getByText('x'))
-    await user.click(screen.getByText('2'))
-    await user.click(screen.getByText('='))
+    await user.click(screen.getByText('6'));
+    await user.click(screen.getByText('x'));
+    await user.click(screen.getByText('2'));
+    await user.click(screen.getByText('='));
 
     expect(screen.getByTestId('calculator-display-inner')).toHaveTextContent('12');
   });
@@ -49,8 +49,8 @@ describe('<Calculator />', () => {
   it('should show decimals correctly', async () => {
     render(<Calculator />);
 
-    await user.click(screen.getByText('●'))
-    await user.click(screen.getByText('2'))
+    await user.click(screen.getByText('●'));
+    await user.click(screen.getByText('2'));
 
     expect(screen.getByTestId('calculator-display-inner')).toHaveTextContent('0.2');
   });
@@ -58,26 +58,26 @@ describe('<Calculator />', () => {
   it('should invert sign correctly', async () => {
     render(<Calculator />);
 
-    await user.click(screen.getByText('2'))
-    await user.click(screen.getByText('±'))
+    await user.click(screen.getByText('2'));
+    await user.click(screen.getByText('±'));
 
     expect(screen.getByTestId('calculator-display-inner')).toHaveTextContent('-2');
   });
 
   it('should apply % correctly', async () => {
     render(<Calculator />);
-    await user.click(screen.getByText('2'))
-    await user.click(screen.getByText('%'))
+    await user.click(screen.getByText('2'));
+    await user.click(screen.getByText('%'));
 
     expect(screen.getByTestId('calculator-display-inner')).toHaveTextContent('0.02');
   });
 
-  it('should clear the display correctly', async() => {
+  it('should clear the display correctly', async () => {
     render(<Calculator />);
-    await user.click(screen.getByText('1'))
-    await user.click(screen.getByText('2'))
-    await user.click(screen.getByText('3'))
-    await user.click(screen.getByText('C'))
+    await user.click(screen.getByText('1'));
+    await user.click(screen.getByText('2'));
+    await user.click(screen.getByText('3'));
+    await user.click(screen.getByText('C'));
 
     expect(screen.getByTestId('calculator-display-inner')).toHaveTextContent('0');
   });
