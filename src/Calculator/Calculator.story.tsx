cb506c8d04f125b5679bfc8ac5bfa373a7f68f1e
KaiHotz <kaihotz@me.com>
2022-08-13 11:59:49 +0400

    dependencies update

--- src/Calculator/Calculator.story.tsx
+++ src/Calculator/Calculator.story.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
+import { Meta } from '@storybook/react';
 import { Calculator } from './Calculator';
 
 export default {
   title: 'Calculator',
   component: Calculator,
-};
+} as Meta;
 
 export const Default: FC = () => {
   return <Calculator />;
