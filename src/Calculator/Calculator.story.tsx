0c25a6ae545171f7b261fa5ba65ba2ba3e958e53
KaiHotz <kaihotz@me.com>
2023-05-12 14:08:28 +0400

    Storybook and dependencies updated

--- src/Calculator/Calculator.story.tsx
+++ src/Calculator/Calculator.story.tsx
@@ -1,12 +1,10 @@
-import React, { FC } from 'react';
-import { Meta } from '@storybook/react';
 import { Calculator } from './Calculator';
 
 export default {
   title: 'Calculator',
   component: Calculator,
-} as Meta;
+};
 
-export const Default: FC = () => {
-  return <Calculator />;
+export const Default = {
+  args: {},
 };
